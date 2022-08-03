25ec48e4b0d2a454e69defabe891d4e8e21e3d47
Chris Hilgenberg <chris.g.hilgenberg@gmail.com>
2022-08-02 20:21:11 -0400

    updated web.xml with project example, added Product class

---
+++ src/main/java/com/example/struts2/struts2example/models/Product.java
@@ -0,0 +1,31 @@
+
+package com.example.struts2.struts2example.models;
+public class Product {
+    private int id;
+    private float price;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
